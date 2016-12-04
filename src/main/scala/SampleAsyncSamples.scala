1755afd21747a1c4d11a3272a5cc789c95ba0bf1
Tudor Zgureanu <tzgureanu@gmail.com>
2016-12-04 20:35:14 +0000

    Add sample computations for the same operation using scala-async, for-comprehension and future API

---
+++ src/main/scala/SampleAsyncSamples.scala
@@ -0,0 +1,27 @@
+import scala.async.Async._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object SampleAsyncSamples {
+
+  val future1: Future[Int] = Future.successful(40)
+  val future2: Future[Int] = Future.successful(2)
+
+  val asyncComputation = async {
+    await(future1) + await(future2)
+  }
+
+  val forComprehensionComputation =
+    for {
+      res1 <- future1
+      res2 <- future2
+    } yield res1 + res2
+
+  val futureAPIComputation =
+    future1.flatMap { res1 =>
+      future2.map { res2 =>
+        res1 + res2
+      }
+    }
+
+}
